ecb70366a5823e7ac8614f066fbd41ff00a82608
Cueto <ncueto2@illinois.edu>
2020-02-29 16:13:37 -0600

    workign on  detect class

--- detector/detector.py
+++ detector/detector.py
@@ -32,120 +32,143 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 # Path to trained weights
 WEIGHTS_PATH = os.path.join(ROOT_DIR, "weights.h5")
 
-# class Detector():
-def apply_cover(image, mask):
-    """Apply cover over image. Based off of Mask-RCNN Balloon color splash function
-    image: RGB image [height, width, 3]
-    mask: instance segmentation mask [height, width, instance count]
+class Detector():
+    # at startup, we only know weights location, and censor type
+    # censor_type: 0 if bar, 1 if mosaic
+    def __init__(self, censorType, weights_path):
+        class InferenceConfig(HentaiConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+        self.config = InferenceConfig()
 
-    Returns result covered image.
-    """
-    # Copy color pixels from the original color image where mask is set
-    # green = np.array([[[0, 255, 0]]], dtype=np.uint8)
-    print('apply_cover: shape of image is',image.shape)
-    green = np.zeros([image.shape[0], image.shape[1], image.shape[2]], dtype=np.uint8)
-    green[:,:] = [0, 255, 0]
-    if mask.shape[-1] > 0:
-        # We're treating all instances as one, so collapse the mask into one layer
-        mask = (np.sum(mask, -1, keepdims=True) < 1)
-        cover = np.where(mask, image, green).astype(np.uint8)
-    else:
-        # error case, return image
-        cover = green.astype(np.uint8)
-    return cover
+        self.weights_path = weights_path
+        self.censor_type = censorType
+        # keep model loading to be done later, not now
+
+    # Make sure this is called before using model weights
+    def load_weights(self):
+        self.model = modellib.MaskRCNN(mode="inference", config=self.config,
+                                      model_dir=DEFAULT_LOGS_DIR)
+        self.model.load_weights(self.weights_path, by_name=True)
+
+    def apply_cover(self, image, mask):
+        """Apply cover over image. Based off of Mask-RCNN Balloon color splash function
+        image: RGB image [height, width, 3]
+        mask: instance segmentation mask [height, width, instance count]
+
+        Returns result covered image.
+        """
+        # Copy color pixels from the original color image where mask is set
+        # green = np.array([[[0, 255, 0]]], dtype=np.uint8)
+        # print('apply_cover: shape of image is',image.shape)
+        green = np.zeros([image.shape[0], image.shape[1], image.shape[2]], dtype=np.uint8)
+        green[:,:] = [0, 255, 0]
+        if mask.shape[-1] > 0:
+            # We're treating all instances as one, so collapse the mask into one layer
+            mask = (np.sum(mask, -1, keepdims=True) < 1)
+            cover = np.where(mask, image, green).astype(np.uint8)
+        else:
+            # error case, return image
+            cover = green.astype(np.uint8)
+        return cover
 
 
-def detect_and_cover(model, image_path=None):
-    assert image_path
-    # Image or video?
-    # if image_path:
-        # Run model detection and generate the color splash effect
-    print("Running on {}".format(image_path))
-    # Read image
-    image = skimage.io.imread(image_path)
-    # Detect objects
-    r = model.detect([image], verbose=1)[0]
-    # Color splash
-    splash = apply_cover(image, r['masks'])
-    # Save output
-    file_name = "cover_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
-    skimage.io.imsave(file_name, splash)
-    '''elif video_path: # TODO: video capabilities will come later
-        import cv2
-        # Video capture
-        vcapture = cv2.VideoCapture(video_path)
-        width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = vcapture.get(cv2.CAP_PROP_FPS)
+    def detect_and_cover(self, image_path=None, save_path=''):
+        assert image_path
+        # Image or video?
+        # if image_path:
+            # Run model detection and generate the color splash effect
+        print("Running on {}".format(image_path))
+        # Read image
+        image = skimage.io.imread(image_path)
+        # Detect objects
+        r = self.model.detect([image], verbose=0)[0]
+        # Color splash
+        cov = self.apply_cover(image, r['masks'])
+        # Save output
+        file_name = save_path + "cover_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
+        skimage.io.imsave(file_name, cov)
+        '''elif video_path: # TODO: video capabilities will come later
+            import cv2
+            # Video capture
+            vcapture = cv2.VideoCapture(video_path)
+            width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = vcapture.get(cv2.CAP_PROP_FPS)
+    
+            # Define codec and create video writer
+            file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
+            vwriter = cv2.VideoWriter(file_name,
+                                      cv2.VideoWriter_fourcc(*'MJPG'),
+                                      fps, (width, height))
+            count = 0
+            success = True
+            while success:
+                print("frame: ", count)
+                # Read next image
+                success, image = vcapture.read()
+                if success:
+                    # OpenCV returns images as BGR, convert to RGB
+                    image = image[..., ::-1]
+                    # Detect objects
+                    r = model.detect([image], verbose=0)[0]
+                    # Color splash
+                    splash = color_splash(image, r['masks'])
+                    # RGB -> BGR to save image to video
+                    splash = splash[..., ::-1]
+                    # Add image to video writer
+                    vwriter.write(splash)
+                    count += 1
+            vwriter.release()'''
+        print("Saved to ", file_name)
 
-        # Define codec and create video writer
-        file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
-        vwriter = cv2.VideoWriter(file_name,
-                                  cv2.VideoWriter_fourcc(*'MJPG'),
-                                  fps, (width, height))
-        count = 0
-        success = True
-        while success:
-            print("frame: ", count)
-            # Read next image
-            success, image = vcapture.read()
-            if success:
-                # OpenCV returns images as BGR, convert to RGB
-                image = image[..., ::-1]
-                # Detect objects
-                r = model.detect([image], verbose=0)[0]
-                # Color splash
-                splash = color_splash(image, r['masks'])
-                # RGB -> BGR to save image to video
-                splash = splash[..., ::-1]
-                # Add image to video writer
-                vwriter.write(splash)
-                count += 1
-        vwriter.release()'''
-    print("Saved to ", file_name)
+    # input input_folder needs trailing slash!!
+    def run_on_folder(self, input_folder, output_folder):
+        assert input_folder
+        assert output_folder # replace with catches and popups
+
+        # obtain inputs from the input folder
+        img_list = []
+        for file in os.listdir(input_folder):
+            # TODO: check what other filetpyes supported
+            if file.endswith(".jpg") or file.endswith('.png') or file.endswith(".JPG") or file.endswith('.PNG') or file.endswith(".jpeg"):
+                img_list.append(os.path.join(input_folder, file))
+
+        # save run detection with outputs to output folder
+        for img in img_list:
+            self.detect_and_cover(img, output_folder)
+
+
 
 # main only used for debugging here. Comment out in prod
 if __name__ == '__main__':
     import argparse
     # Parse command line arguments
     parser = argparse.ArgumentParser(
-        description='Train Mask R-CNN to detect censor bars.')
-    parser.add_argument("command",
-                        metavar="<command>",
-                        help="just 'cover'")
+        description='Utilize Mask R-CNN to detect censor bars.')
     parser.add_argument('--weights', required=True,
                         metavar="/path/to/weights.h5",
-                        help="Path to weights .h5 file or 'coco'")
-    parser.add_argument('--image', required=False,
-                        metavar="path or URL to image",
-                        help='Image to apply the color splash effect on')
+                        help="Path to weights.h5")
+    parser.add_argument('--imagedir', required=True,
+                        metavar="path to image folder",
+                        help='Folder of images to apply mask coverage on')
     # parser.add_argument('--video', required=False,
     #                     metavar="path or URL to video",
     #                     help='Video to apply the color splash effect on')
     args = parser.parse_args()
 
-    # Validate argument
-    if args.command == "cover":
-        assert args.image
     weights_path = args.weights
+    images_path = args.imagedir
+    is_mosaic = 0
+    output_dir = "temp_out/"
 
-    class InferenceConfig(HentaiConfig):
-        # Set batch size to 1 since we'll be running inference on
-        # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-        GPU_COUNT = 1
-        IMAGES_PER_GPU = 1
-    config = InferenceConfig()
+    print('Initializing Detector class')
+    detect_instance = Detector(censorType=is_mosaic, weights_path=args.weights)
+    print('loading weights')
+    detect_instance.load_weights()
+    print('running detect on in and out folder')
+    detect_instance.run_on_folder(input_folder=images_path, output_folder=output_dir)
 
-    print("Loading weights ", weights_path)
-    model = modellib.MaskRCNN(mode="inference", config=config,
-                                  model_dir=DEFAULT_LOGS_DIR)
-    model.load_weights(weights_path, by_name=True)
-    print("model loaded")
-    if args.command == "cover":
-        # detect_and_cover(model, image_path='detectorTest1_o')
-        detect_and_cover(model, image_path='detector\detectTest2_o.jpg')
-        detect_and_cover(model, image_path='detector\detectTest3_o.jpg')
-        detect_and_cover(model, image_path='detector\detectTest4_o.jpg')
-    else:
-        print("'{}' is not recognized. "
-              "Use 'cover'".format(args.command))
+    print("Fin")
